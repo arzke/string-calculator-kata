430f3b58e11cac73409012909736941119af5bb2
Bruno Arsene <bruno.arsene@ignitionone.com>
2018-02-28 15:20:34 +0100

    Delegate numbers filtering to a specific method

--- calculator.py
+++ calculator.py
@@ -5,18 +5,20 @@ from re import split
 class StringCalculator:
     def add(self, values):
         delimiters, values = self._extract_delimiters(values)
-        numbers = list(map(lambda x: int(x or 0), split(delimiters, values)))
+        numbers = map(lambda x: int(x or 0), split(delimiters, values))
+        filtered_numbers = self._filter(list(numbers))
 
-        negative_numbers = list(filter(lambda x: x < 0, numbers))
-        if len(negative_numbers) > 0:
-            raise ValueError('negatives not allowed: ' + ', '.join(map(str, negative_numbers)))
-
-        numbers_smaller_than_1000 = filter(lambda x: x <= 1000, numbers)
-
-        return sum(numbers_smaller_than_1000)
+        return sum(filtered_numbers)
 
     def _extract_delimiters(self, values):
         if values.startswith('//'):
             delimiters, values = values.split('\n', 1)
             return delimiters[2:].replace('][', ']|[') + '|,|\n', values
         return ',|\n', values
+
+    def _filter(self, numbers):
+        negative_numbers = list(filter(lambda x: x < 0, numbers))
+        if len(negative_numbers) > 0:
+            raise ValueError('negatives not allowed: ' + ', '.join(map(str, negative_numbers)))
+
+        return filter(lambda x: x <= 1000, numbers)
