b2573fe1ee657b6c42c2202b798b533f475bd2aa
Bruno Arsene <bruno.arsene@ignitionone.com>
2018-02-28 15:17:05 +0100

    Replace double underscore with single underscore for internal method

--- calculator.py
+++ calculator.py
@@ -4,7 +4,7 @@ from re import split
 
 class StringCalculator:
     def add(self, values):
-        delimiters, values = self.__extract_delimiters(values)
+        delimiters, values = self._extract_delimiters(values)
         numbers = list(map(lambda x: int(x or 0), split(delimiters, values)))
 
         negative_numbers = list(filter(lambda x: x < 0, numbers))
@@ -15,7 +15,7 @@ class StringCalculator:
 
         return sum(numbers_smaller_than_1000)
 
-    def __extract_delimiters(self, values):
+    def _extract_delimiters(self, values):
         if values.startswith('//'):
             delimiters, values = values.split('\n', 1)
             return delimiters[2:].replace('][', ']|[') + '|,|\n', values
